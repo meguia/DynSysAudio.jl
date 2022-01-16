f83fed36ab5c5b7ef5584f63f11da1cdff5a8203
meguia <eguiamc@gmail.com>
2022-01-16 16:46:34 -0300

    Update DynSysAudio.jl

--- src/DynSysAudio.jl
+++ src/DynSysAudio.jl
@@ -4,12 +4,14 @@ A Julia package for making sound from dynamical systems
 module DynSysAudio
 
     using SampledSignals
+    using DifferentialEquations
     using Unitful
 
     import SampledSignals: nchannels, samplerate, unsafe_read!
 
     export  NoiseSource,
             SinusoidSource,
+            ODESource,
             samplerate
 
     include("NoiseSource.jl")
