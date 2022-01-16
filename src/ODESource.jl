7f9037ae2c2c0c2d91be100a34014fbf8c2006fe
meguia <eguiamc@gmail.com>
2022-01-16 19:08:45 -0300

    Update ODESource.jl

--- src/ODESource.jl
+++ src/ODESource.jl
@@ -9,17 +9,17 @@ end
 
 # ODE EXAMPLE
 function vdp!(du,u,p,t)
-	du[1] = u[2]
-	du[2] = p[1]*(1.0-u[1]*u[1])*u[2]-u[1]
+    du[1] = u[2]
+    du[2] = p[1]*(1.0-u[1]*u[1])*u[2]-u[1]
 end	
 
-function ODESource(eltype, samplerate, timescale, pars::Array)
+function ODESource(eltype, samplerate::Number, timescale::Number, pars::Array)
     u0 = [0.1, 0.1]
-	tspan = (0.0, 100.0)
-	problem = ODEProblem(vdp!,u0,tspan,pars)
+    tspan = (0.0, 100.0)
+    problem = ODEProblem(vdp!,u0,tspan,pars)
     dt = timescale/samplerate
     integrator = init(problem,Tsit5())
-    new{eltype}(Float64(samplerate), timescale, pars)
+    ODESource{eltype}(Float64(samplerate), timescale, dt, pars, problem,integrator)
 end
 
 Base.eltype(::ODESource{T}) where T = T
@@ -30,6 +30,6 @@ function unsafe_read!(source::ODESource, buf::Array, frameoffset, framecount)
     tini = frameoffset*source.dt
     tend = tini+(framecount-1)*source.dt
     seq = TimeChoiceIterator(source.integrator,tini:source.dt:tend)
-	buf[frameoffset+1:frameoffset+framecount,1] = [u[1] for (u,t) in seq]
-	framecount
+    buf[frameoffset+1:frameoffset+framecount,1] = [u[1] for (u,t) in seq]
+    framecount
 end
