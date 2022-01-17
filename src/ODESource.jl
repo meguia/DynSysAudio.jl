e4bf53418a043f9aca6a8ff6a1a0b9b1cf1b57be
meguia <eguiamc@gmail.com>
2022-01-17 14:48:56 -0300

    Update ODESource.jl

--- src/ODESource.jl
+++ src/ODESource.jl
@@ -3,19 +3,21 @@ mutable struct ODESource{T} <: SampleSource
     nchannels::Int
     time::Float64
     dt::Float64
+    gain::FLoat64
     problem::OrdinaryDiffEq.ODEProblem
     integrator::OrdinaryDiffEq.ODEIntegrator
 end
 
 
 function ODESource(eltype, system::Function, samplerate::Number, timescale::Number, start_point::Array, pars::Array)
-    nchannels = 1
+    nchannels = length(start_point)
     time = 0.0
+    gain = 1.0
     tspan = (0.0, 100.0)
     problem = ODEProblem(system,start_point,tspan,pars)
     dt = timescale/samplerate
     integrator = init(problem,Tsit5())
-    ODESource{eltype}(Float64(samplerate), nchannels, time, dt, problem,integrator)
+    ODESource{eltype}(Float64(samplerate), nchannels, time, dt, gain,problem,integrator)
 end
 
 Base.eltype(::ODESource{T}) where T = T
@@ -25,7 +27,9 @@ samplerate(source::ODESource) = source.samplerate
 function unsafe_read!(source::ODESource, buf::Array, frameoffset, framecount)
     tend = source.time+(framecount-1)*source.dt
     seq = TimeChoiceIterator(source.integrator,source.time:source.dt:tend)
-    buf[frameoffset+1:frameoffset+framecount,1] = [u[1] for (u,t) in seq]
+    for n=1:nchannels
+        buf[frameoffset+1:frameoffset+framecount,n] = [source.gain*u[n] for (u,t) in seq]
+    end    
     source.time += framecount*source.dt
     framecount
 end
